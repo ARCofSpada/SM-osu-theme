4bac92698f1f5379c6ce33d8e76f8a3ccc56e067
Sirius <kirbyrulez@att.net>
2016-12-01 22:24:47 -0600

    sound

--- Scripts/SpriteUtil.lua
+++ Scripts/SpriteUtil.lua
@@ -14,3 +14,12 @@ function loadOsuGraphic(fileName)
             return THEME:GetPathG("", "_fallback/"..fileName..".png")
         end;
 end
+
+function loadOsuSound(fileName)
+    --local path = THEME:GetCurrentThemeDirectory().."Graphics/_skin/"
+    if FILEMAN:DoesFileExist(THEME:GetPathG("", "_skin/"..fileName) then --Would it kill them to create a sounds folder or something? Fuck osu
+        return THEME:GetPathG("", "_skin/"..fileName)
+    else
+        return THEME:GetPathG("", "_fallback/"..fileName)
+    end;
+end;
---
+++ Sounds/Common start.lua
@@ -0,0 +1,1 @@
+return loadOsuSound(click-short);
